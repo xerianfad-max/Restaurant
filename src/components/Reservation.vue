49bc9f301d3cd88cc9b9d9d5e0386f4938ad640b
unknown <Xerianfad@gmail.com>
2025-09-24 12:14:29 +0300

    second commit

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -1,6 +1,68 @@
 <script setup>
+
 </script>
 
 <template>
     <h1>Reservation</h1>
+
+<v-container>
+    <v-row>
+        <v-col>
+            <v-card max-width="600">
+                <v-row>
+                        <p class="text-h5">Make a reservation</p>
+                    </v-row>
+                    <v-row>
+                        <v-col md="4">Name</v-col>
+                        <v-col md="8">
+                            <v-text-field label="Name" ></v-text-field>
+                        </v-col>
+                    </v-row>
+                   <v-row>
+                        <v-col md="4">Phone</v-col>
+                        <v-col md="8">
+                            <v-text-field label="2541234456" ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="4">No. of People</v-col>
+                        <v-col md="8">
+                            <v-text-field></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="4">Date</v-col>
+                        <v-col md="8">
+                            <v-text-field></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="4">Time</v-col>
+                        <v-col md="8">
+                            <v-text-field></v-text-field>
+                        </v-col>
+                        <v-row>
+                        <v-col md="4">Location</v-col>
+                        <v-col md="8">
+                            <v-radio-group>
+                            <v-row>
+                                <v-col md="4">
+                                    <v-radio label="Madaraka" value="Madaraka"></v-radio>
+                                </v-col>
+                                <v-col md="4">
+                                    <v-radio label="CBD" value="CBD"></v-radio>
+                                </v-col>
+                                <v-col md="4">
+                                    <v-radio label="Your Home" value="Your Home"></v-radio>
+                                </v-col>
+                            </v-row>
+                            </v-radio-group>
+                        </v-col>
+                    </v-row>
+                    </v-row>
+
+            </v-card>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
